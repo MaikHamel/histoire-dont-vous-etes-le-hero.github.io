8329d73ed5ea57a2b4c4329c15674014d5e3a315
MaikHamel <hamm3157@gmail.com>
2023-09-26 15:00:39 -0400

    continuation javascript
    conyinuation des chapitres javascript

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
 let chapters = {
   debut: {
-    titre: 'intro',
-    description: 'Vous, une elfe, voulez vous venger du roi du royaume voisin pour avoir détruit votre village. Assoiffé de vegence, vous arrivez au royaume du roi',
-    image:,
-    boutons:[],
-  },
-
-  taverne: {
-    titre:'Devant la taverne',
-    description: 'En vous promenant dans le gros village du royaume, vous voyez une taverne et trouvez que vous pourriez avoir des informations à l`intérieur',
-    image:,
-    boutons:[],
+    titre: "intro",
+    description:
+      "Vous, une elfe, voulez vous venger du roi du royaume voisin pour avoir détruit votre village. Assoiffé de vegence, vous arrivez au royaume du roi. En vous promenant dans le gros village du royaume, vous voyez une taverne et trouvez que vous pourriez avoir des informations à l'intérieur",
+    image: "",
+    boutons: [],
   },
 
   bandits: {
-    titre:'Question de territoire',
-    description: 'En prenant la décision de ne pas entrer, vous arrivez dans une ruelle, qui est occupé par des bandits. Vous vous faites tuer par un des bandits',
-    image:,
-    boutons:[],
+    titre: "Question de territoire",
+    description:
+      "En prenant la décision de ne pas entrer, vous arrivez dans une ruelle, qui est occupé par des bandits. Vous vous faites tuer par un des bandits",
+    image: "",
+    boutons: [],
   },
 
   entrer: {
-    titre:'',
-    description: ''
-  }
-
-
+    titre: "La taverne",
+    description:
+      "Vous décidez d'entrer à l'intérieur de la taverne. L'intérieur de la taverne est grand: le coté droite contient une vingtaine de tables qui tous occupés et à la gauche, il y a un bar avec un comptoir pouvant accueillir une trentaine de personnes. En avancant tranquillement dans la taverne, esseillant d'oublier les multiples paires de yeux qui vous regarde, vous voyez une paire de dagues trainant dans un coin ",
+    image: "",
+    boutons: [],
+  },
 };
