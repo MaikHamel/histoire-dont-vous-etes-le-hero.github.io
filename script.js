d979f24db12540346129f74ecec223120849121e
MaikHamel <hamm3157@gmail.com>
2023-09-26 13:51:50 -0400

    ajout javascript
    début du code javascript a propos des chapitres

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 let chapters = {
   debut: {
-    titre: 'intro';
-    description: ''
+    titre: 'intro',
+    description: 'Vous, une elfe, voulez vous venger du roi du royaume voisin pour avoir détruit votre village. Assoiffé de vegence, vous arrivez au royaume du roi',
+    image:,
+    boutons:[],
   },
+
+  taverne: {
+    titre:'Devant la taverne',
+    description: 'En vous promenant dans le gros village du royaume, vous voyez une taverne et trouvez que vous pourriez avoir des informations à l`intérieur',
+    image:,
+    boutons:[],
+  },
+
+  bandits: {
+    titre:'Question de territoire',
+    description: 'En prenant la décision de ne pas entrer, vous arrivez dans une ruelle, qui est occupé par des bandits. Vous vous faites tuer par un des bandits',
+    image:,
+    boutons:[],
+  },
+
+  entrer: {
+    titre:'',
+    description: ''
+  }
+
+
 };
