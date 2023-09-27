999d55089364c3b84a22acbfdcb0f569b25ff1b1
MaikHamel <hamm3157@gmail.com>
2023-09-27 16:41:51 -0400

    ajout des boutons
    ajout des boutons avec javascript

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ let chapters = {
     description:
       "Vous, une elfe, voulez vous venger du roi du royaume voisin pour avoir détruit votre village. Assoiffé de vegence, vous arrivez au royaume du roi. En vous promenant dans le gros village du royaume, vous voyez une taverne et trouvez que vous pourriez avoir des informations à l'intérieur",
     image: "",
-    boutons: [],
+    boutons: [
+      { titre: "Entrer à l'intérieur", destination: "entrer" },
+      { titre: "Ne pas entrer à l'intérieur", destination: "bandits" },
+    ],
   },
 
   bandits: {
@@ -12,7 +15,7 @@ let chapters = {
     description:
       "En prenant la décision de ne pas entrer, vous arrivez dans une ruelle, qui est occupé par des bandits. Vous vous faites tuer par un des bandits",
     image: "",
-    boutons: [],
+    boutons: [{ titre: "recomencer", destination: "debut" }],
   },
 
   entrer: {
@@ -20,7 +23,10 @@ let chapters = {
     description:
       "Vous décidez d'entrer à l'intérieur de la taverne. L'intérieur de la taverne est grand: le coté droite contient une vingtaine de tables qui tous occupés et à la gauche, il y a un bar avec un comptoir pouvant accueillir une trentaine de personnes. En avancant tranquillement dans la taverne, esseillant d'oublier les multiples paires de yeux qui vous regarde, vous voyez une paire de dagues trainant dans un coin ",
     image: "",
-    boutons: [],
+    boutons: [
+      { titre: "Les prendre", destination: "comptoir" },
+      { titre: "Ne pas les prendre", destination: "comptoir" },
+    ],
   },
 
   comptoir: {
@@ -28,7 +34,10 @@ let chapters = {
     description:
       "Après, vous décider d'aller vers le comptoir du bar. Au comptoir, vous voyez qu'un soldat, au regard intimidant, est assit à coté de vous. Vous hesiter entre l'intimider pour montrer votre dominance et avoir de l'information ou lui parler poliment pour avoir de l'information",
     image: "",
-    boutons: [],
+    boutons: [
+      { titre: "Lui parler poliment", destination: "informations" },
+      { titre: "L'intimider", destination: "intimidation" },
+    ],
   },
 
   intimidation: {
@@ -36,7 +45,7 @@ let chapters = {
     description:
       "Prenant vos insultes personnel, il décide de vous étrangler jusqu'à votre mort",
     image: "",
-    boutons: [],
+    boutons: [{ titre: "recomencer", destination: "debut" }],
   },
 
   informations: {
@@ -44,7 +53,10 @@ let chapters = {
     description:
       "Il vous répond calmement que celui-ci sait seulement que le roi est présentement dans le chateau. Vous décidez donc d'aller au château. AU chateau, vous entendez que le roi est dans sa chambre",
     image: "",
-    boutons: [],
+    boutons: [
+      { titre: "Aller vers la chambre", destination: "chambre" },
+      { titre: "Explorer le château", destination: "soldats" },
+    ],
   },
 
   soldat: {
@@ -52,7 +64,7 @@ let chapters = {
     description:
       "En décidant de vous d'explorer les intérieurs du château avant d'aller dans la chambre du roi, vous croisez in soldat qui vous pose beaucoup de questions. Cependant vous hésitez à plusieurs de ses questions. par panique vous essayer de vous enfuir, mais il vous rattrappe et vous emprisonne.",
     image: "",
-    boutons: [],
+    boutons: [{ titre: "recomencer", destination: "debut" }],
   },
 
   chambre: {
@@ -60,7 +72,10 @@ let chapters = {
     description:
       "Arrivé à la chambre du roi, vous décidez d'entrer à l'intérieur. Le roi est bien dans sa chambre et vous regarde d'une maniere arrogant et machiavélique, comme s'il savait que vous vienderiez. Vous pouvez enfin acomplir votre mission. Avez vous les dagues ?",
     image: "",
-    boutons: [],
+    boutons: [
+      { titre: "Oui", destination: "reussite" },
+      { titre: "Non", destination: "echec" },
+    ],
   },
 
   echec: {
@@ -68,7 +83,7 @@ let chapters = {
     description:
       "N'ayant pas d'arme pour le tuer, le roi appelle ses gardes et vous emprisonne pour le reste de votre vie",
     image: "",
-    boutons: [],
+    boutons: [{ titre: "recomencer", destination: "debut" }],
   },
 
   reussite: {
@@ -76,6 +91,5 @@ let chapters = {
     description:
       "L'heure de la vengence a sonné, vous utilisez vos deux dagues pour lui poignarder le coeur. Après vous vous enfuyez, satisfaite de votre vengence",
     image: "",
-    boutons: [],
   },
 };
