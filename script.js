03dc85dd998ff78ee483cae6a706b58207f23be5
MaikHamel <hamm3157@gmail.com>
2023-09-28 13:36:21 -0400

    finalisation des chapitre en javascript

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let chapters = {
   debut: {
     titre: "intro",
     description:
-      "Vous, une elfe, voulez vous venger du roi du royaume voisin pour avoir détruit votre village. Assoiffé de vegence, vous arrivez au royaume du roi. En vous promenant dans le gros village du royaume, vous voyez une taverne et trouvez que vous pourriez avoir des informations à l'intérieur",
+      "Vous, une elfe, voulez vous venger du roi du royaume voisin pour avoir détruit votre village. Assoiffée de vegence, vous arrivez au royaume du roi. En vous promenant dans le gros village du royaume, vous voyez une taverne et trouvez que vous pourriez avoir des informations à l'intérieur",
     image: "",
     boutons: [
       { titre: "Entrer à l'intérieur", destination: "entrer" },
@@ -13,7 +13,7 @@ let chapters = {
   bandits: {
     titre: "Question de territoire",
     description:
-      "En prenant la décision de ne pas entrer, vous arrivez dans une ruelle, qui est occupé par des bandits. Vous vous faites tuer par un des bandits",
+      "En prenant la décision de ne pas entrer, vous arrivez dans une ruelle, qui est occupée par des bandits. Vous vous faites tuer par un des bandits",
     image: "",
     boutons: [{ titre: "recomencer", destination: "debut" }],
   },
@@ -21,7 +21,7 @@ let chapters = {
   entrer: {
     titre: "La taverne",
     description:
-      "Vous décidez d'entrer à l'intérieur de la taverne. L'intérieur de la taverne est grand: le coté droite contient une vingtaine de tables qui tous occupés et à la gauche, il y a un bar avec un comptoir pouvant accueillir une trentaine de personnes. En avancant tranquillement dans la taverne, esseillant d'oublier les multiples paires de yeux qui vous regarde, vous voyez une paire de dagues trainant dans un coin ",
+      "Vous décidez d'entrer à l'intérieur de la taverne. L'intérieur de la taverne est grand: le coté droite contient une vingtaines de tables qui tous occupées et à la gauche, il y a un bar avec un comptoir pouvant accueillir une trentaine de personnes. En avancant tranquillement dans la taverne, vous voyez une paire de dagues trainant dans un coin ",
     image: "",
     boutons: [
       { titre: "Les prendre", destination: "comptoir" },
@@ -32,7 +32,7 @@ let chapters = {
   comptoir: {
     titre: "Au comptoir",
     description:
-      "Après, vous décider d'aller vers le comptoir du bar. Au comptoir, vous voyez qu'un soldat, au regard intimidant, est assit à coté de vous. Vous hesiter entre l'intimider pour montrer votre dominance et avoir de l'information ou lui parler poliment pour avoir de l'information",
+      "Vous décidez d'aller vers le comptoir du bar. Au comptoir, vous voyez qu'un soldat, au regard intimidant, est assit à coté de vous. Vous hesitez entre l'intimider pour montrer votre dominance et avoir de l'information ou lui parler poliment pour avoir de l'information",
     image: "",
     boutons: [
       { titre: "Lui parler poliment", destination: "informations" },
@@ -51,7 +51,7 @@ let chapters = {
   informations: {
     titre: "L'information du soldat",
     description:
-      "Il vous répond calmement que celui-ci sait seulement que le roi est présentement dans le chateau. Vous décidez donc d'aller au château. AU chateau, vous entendez que le roi est dans sa chambre",
+      "Il vous répond calmement que celui-ci sait seulement que le roi est présentement dans le chateau. Vous décidez donc d'aller au château. Au château, vous entendez que le roi est dans sa chambre",
     image: "",
     boutons: [
       { titre: "Aller vers la chambre", destination: "chambre" },
@@ -62,7 +62,7 @@ let chapters = {
   soldat: {
     titre: "Exploration du château",
     description:
-      "En décidant de vous d'explorer les intérieurs du château avant d'aller dans la chambre du roi, vous croisez in soldat qui vous pose beaucoup de questions. Cependant vous hésitez à plusieurs de ses questions. par panique vous essayer de vous enfuir, mais il vous rattrappe et vous emprisonne.",
+      "En décidant de vous d'explorer les intérieurs du château avant d'aller dans la chambre du roi, vous croisez in soldat qui vous pose beaucoup de questions. Cependant, vous hésitez à plusieurs de ses questions. Par panique vous essayer de vous enfuir, mais il vous rattrappe et vous emprisonne.",
     image: "",
     boutons: [{ titre: "recomencer", destination: "debut" }],
   },
@@ -70,7 +70,7 @@ let chapters = {
   chambre: {
     titre: "Arrivé à la chambre",
     description:
-      "Arrivé à la chambre du roi, vous décidez d'entrer à l'intérieur. Le roi est bien dans sa chambre et vous regarde d'une maniere arrogant et machiavélique, comme s'il savait que vous vienderiez. Vous pouvez enfin acomplir votre mission. Avez vous les dagues ?",
+      "Arrivée à la chambre du roi, vous décidez d'entrer à l'intérieur. Le roi est bien dans sa chambre et vous regarde d'une maniere arrogant et machiavélique, comme s'il savait que vous vienderiez. Vous pouvez enfin acomplir votre mission. Avez vous les dagues ?",
     image: "",
     boutons: [
       { titre: "Oui", destination: "reussite" },
@@ -89,7 +89,7 @@ let chapters = {
   reussite: {
     titre: "L'heure de la vengence",
     description:
-      "L'heure de la vengence a sonné, vous utilisez vos deux dagues pour lui poignarder le coeur. Après vous vous enfuyez, satisfaite de votre vengence",
+      "L'heure de la vengence a sonné, vous utilisez vos deux dagues pour lui poignarder la poitrine. Après, vous vous enfuyez, satisfaite de votre vengence",
     image: "",
   },
 };
@@ -104,8 +104,15 @@ let goTochapter = function (chapitre) {
         `${chapters[chapitre].boutons[i].titre} \n ${chapters[chapitre].boutons[i].destination}`
       );
     }*/
-    console.log(chapters[chapitre].boutons[0]);
-    console.log(chapters[chapitre].boutons[1]);
+
+    console.log("options: ");
+    for (let i = 0; i < chapters[chapitre].boutons.length; i++) {
+      console.log(
+        `${chapters[chapitre].boutons[i].titre} => goTochapter("${chapters[chapitre].boutons[i].destination}")`
+      );
+    }
+    /*console.log(chapters[chapitre].boutons[0]);
+    console.log(chapters[chapitre].boutons[1]);*/
   } else {
     console.log("mauvais cle de chapitre chapitre");
   }
